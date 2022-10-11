80a22fe2f478a412286c1935cc1135226fbb9e5c
Avinash Kumar Ray <96744787+Avi1702@users.noreply.github.com>
2022-10-11 14:32:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const fs=require("fs/promises")
+
+async function start(){
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/search?q=react+jobs&ei=4duYYpLRJYq94-EPqfK90AE&uact=5&oq=react+jobs&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMggIABCABBDJAzIFCAAQkgMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgQIABBHOgUIABCRAjoUCC4QgAQQsQMQgwEQxwEQ0QMQ1AI6CAguEIAEELEDOgUILhCABDoLCAAQgAQQsQMQgwE6BAgAEEM6DgguEIAEELEDEMcBEKMCOgsILhDHARCvARCRAjoHCAAQyQMQQzoOCC4QgAQQsQMQxwEQ0QM6CAgAEIAEELEDOggILhCABBDUAjoLCC4QgAQQsQMQ1AI6DQgAELEDEIMBEMkDEEM6CAgAELEDEIMBOgoIABCxAxCDARANOgQIABANOgcIABDJAxANSgQIQRgASgQIRhgAUJseWIgzYOczaANwAngAgAGmAYgB5AqSAQQwLjExmAEAoAEBsAEAyAEIwAEB&sclient=gws-wiz&ibp=htl;jobs&sa=X&ved=2ahUKEwi546uOj4_4AhXh6zgGHQlPDQYQutcGKAF6BAgHEAY#htivrt=jobs&htidocid=DNXJc0LPw_4AAAAAAAAAAA%3D%3D&fpstate=tldetail');
+  await page.screenshot({path:"google.png"})
+  // Get the "viewport" of the page, as reported by the page.
+  const data = await page.evaluate(() => {
+    // let role= 
+   return  Array.from(document.querySelectorAll(".oNwCmf .vNEEBe")).map(x=>x.textContent)
+    // const array=[...role]
+    // console.log(array)
+    // return array.map((title)=>{title.innerText})
+}) 
+  await fs.writeFile("heading.txt",data.join("\r\n"))
+   console.log(data)
+   await browser.close()
+
+  }
+
+  start()
